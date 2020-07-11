ab556b97a50aa54578336e5ebb99413981911bdb
sys09270883 <50787869+sys09270883@users.noreply.github.com>
2020-07-11 14:20:38 +0900

    Add admin.py

--- mysite/bookmark/admin.py
+++ mysite/bookmark/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from .models import Bookmark
 
 
-@admin.register(Bookmark)
 class BookmarkAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'url')
+
+
+admin.site.register(Bookmark, BookmarkAdmin)
