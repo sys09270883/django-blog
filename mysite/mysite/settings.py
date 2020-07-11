472c7d18ede3932e821964ce46a00044fc1c34ff
sys09270883 <50787869+sys09270883@users.noreply.github.com>
2020-07-11 14:52:37 +0900

    Create blog app

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'bookmark.apps.BookmarkConfig',
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
