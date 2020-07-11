389b469122e8c121dfea9266d9dc41ae345a05c6
sys09270883 <50787869+sys09270883@users.noreply.github.com>
2020-07-11 15:17:18 +0900

    Add blog url

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('bookmark/', include('bookmark.urls')),
+    path('blog/', include('blog.urls')),
 ]
