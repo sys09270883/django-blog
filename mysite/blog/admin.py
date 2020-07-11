3b8eb43c62e040d7ccd026c61ae67afbc7acee23
sys09270883 <50787869+sys09270883@users.noreply.github.com>
2020-07-11 15:04:03 +0900

    Add blog model

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'modify_dt')
+    list_filter = ('modify_dt',)
+    search_fields = ('title', 'content')
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Post, PostAdmin)
