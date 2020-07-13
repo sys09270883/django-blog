3520215972c7a4e6e4ee22ce0d8f6e335ee8204d
Yoonseop Shin <sys09270883@gmail.com>
2020-07-13 10:09:40 +0900

    Add search url, form, view

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -67,7 +67,6 @@ class SearchFormView(FormView):
 	def form_valid(self, form):
 		searchWord = form.cleaned_data['search_word']
 		post_list = Post.objects.filter(Q(title__icontains=searchWord) | Q(description__icontains=searchWord) | Q(content__icontains=searchWord).distinct())
-
 		context = {}
 		context['form'] = form
 		context['search_term'] = searchWord
