6c6ca6a7ba781cd67a6967a8cc26d5e38681c790
sys09270883 <50787869+sys09270883@users.noreply.github.com>
2020-07-15 23:16:18 +0900

    Add mixin and views

--- mysite/bookmark/views.py
+++ mysite/bookmark/views.py
@@ -1,5 +1,8 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 from .models import Bookmark
+from mysite.mysite.views import OwnerOnlyMixin
 
 
 class BookmarkLV(ListView):
@@ -8,3 +11,31 @@ class BookmarkLV(ListView):
 
 class BookmarkDV(DetailView):
     model = Bookmark
+
+
+class BookmarkCreateView(LoginRequiredMixin, CreateView):
+    model = Bookmark
+    fields = ['title', 'url']
+    success_url = reverse_lazy('bookmark:index')
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+
+class BookmarkChangeLV(LoginRequiredMixin, ListView):
+    template_name = 'bookmark/bookmark_change_list.html'
+
+    def get_queryset(self):
+        return Bookmark.objects.filter(owner=self.request.user)
+
+
+class BookmarkUpdateView(OwnerOnlyMixin, UpdateView):
+    model = Bookmark
+    fields = ['title', 'url']
+    success_url = reverse_lazy('bookmark:index')
+
+
+class BookmarkDeleteView(OwnerOnlyMixin, DeleteView):
+    model = Bookmark
+    success_url = reverse_lazy('bookmark:index')
--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import TemplateView, CreateView
 from django.urls import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import AccessMixin
 
 
 class HomeView(TemplateView):
@@ -15,3 +16,14 @@ class UserCreateView(CreateView):
 
 class UserCreateDoneTV(TemplateView):
     template_name = 'registration/register_done.html'
+
+
+class OwnerOnlyMixin(AccessMixin):
+    raise_exception = True
+    permission_denied_message = "Owner only can update/delete the object"
+
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if request.user != obj.owner:
+            return self.handle_no_permission()
+        return super().dispatch(request, *args, **kwargs)
